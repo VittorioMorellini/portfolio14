c7fa635c0aa102f0a1d36ae1070c0be255932aca
Vittorio Morellini <sxi0147@sixtema.it>
2023-05-26 17:37:35 +0200

    Adding post Support renaming

--- lib/postSupport.ts
+++ lib/postSupport.ts
@@ -35,18 +35,3 @@ export async function getPost(id: number) {
     return post
 }
 
-export async function getPostsMeta(): Promise<Meta[] | undefined> {
-    const res = await fetch('https://api.github.com/repos/gitdagray/test-blogposts/git/trees/main?recursive=1', {
-        headers: {
-            Accept: 'application/vnd.github+json',
-            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-    }) 
-
-    if (!res.ok) return undefined
-
-    const repoFileTree: Filetree = await res.json()
-    const filesArray = repoFileTree.tree.map(obj => obj.path).filter((path: string) => path.endsWith('.mdx'))
-}
-  
